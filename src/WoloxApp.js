75f6a36790a57e56d78edb3f9ca0f575dc4b5d89
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-09-28 12:26:54 -0300

    Create structure of project and some initial files.

---
+++ src/WoloxApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './WoloxApp.module.scss';
+
+const WoloxApp = () => {
+    return (
+        <div className={styles.app}>
+            <h1>Wolox</h1>
+        </div>
+    )
+}
+
+export default WoloxApp
